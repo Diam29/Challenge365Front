dde16235019375606ffe9b80561a9a76430f4889
Diamela <diamelavlioi@gmail.com>
2024-04-09 08:50:59 -0300

    change wtp

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import { FaGithub } from "react-icons/fa";
 import { FaRegRegistered } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { Link } from "react-icons/fa";
 import styles from "./Footer.module.css";
 const Footer = () => {
+
+  const handleWhatsappClick = () => {
+    const phoneNumber = '+5492234546521';
+    const message = '¡Hola! ¿Cómo estás?';
+    window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
+  };
   return (
     <div className={styles.container}>
       <div className={styles.contain_icons}> 
-<span className={styles.icon}><FaGithub /> </span>  <span className={styles.icon}><IoLogoWhatsapp /> </span>  
+      <Link href='https://github.com/Diam29' target="_blank" className={styles.icon}><FaGithub /></Link>
+<Link href={'https://api.whatsapp.com/send?phone=5492234546521&text=Hola te escribo desde Challenge365b!!'} target="_blank" className={styles.icon} onClick={handleWhatsappClick}><IoLogoWhatsapp /> </Link>
 
       </div>
 
