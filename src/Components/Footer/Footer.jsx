436dbeb0e0ba953ceb9dabd5e8e7a5dead2767ec
Diamela <diamelavlioi@gmail.com>
2024-04-09 08:59:49 -0300

    change footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import { FaGithub } from "react-icons/fa";
 import { FaRegRegistered } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
-import { Link } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 const Footer = () => {
 
@@ -12,13 +12,13 @@ const Footer = () => {
   };
   return (
     <div className={styles.container}>
-      <div className={styles.contain_icons}> 
-      <Link href='https://github.com/Diam29' target="_blank" className={styles.icon}><FaGithub /></Link>
-<Link href={'https://api.whatsapp.com/send?phone=5492234546521&text=Hola te escribo desde Challenge365b!!'} target="_blank" className={styles.icon} onClick={handleWhatsappClick}><IoLogoWhatsapp /> </Link>
+      <div className={styles.contain_icons}>
+        <Link href='https://github.com/Diam29' target="_blank" className={styles.icon}><FaGithub /></Link>
+        <Link href={'https://api.whatsapp.com/send?phone=5492234546521&text=Hola te escribo desde Challenge365b!!'} target="_blank" className={styles.icon} onClick={handleWhatsappClick}><IoLogoWhatsapp /> </Link>
 
       </div>
 
-       <h3 className={styles.copyright}>copyright <FaRegRegistered />  2024 - DIAMELA VILLALBA</h3>
+      <h3 className={styles.copyright}>copyright <FaRegRegistered />  2024 - DIAMELA VILLALBA</h3>
     </div>
   )
 }
